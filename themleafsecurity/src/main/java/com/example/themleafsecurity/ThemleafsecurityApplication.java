d8ceb607e5b10e72f4600bc51978a5edbc0b3fa3
wangkun <kunwang1@jieyuechina.com>
2018-08-29 15:16:39 +0800

    init

---
+++ themleafsecurity/src/main/java/com/example/themleafsecurity/ThemleafsecurityApplication.java
@@ -0,0 +1,12 @@
+package com.example.themleafsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThemleafsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThemleafsecurityApplication.class, args);
+	}
+}
