1190e476c65a0867dd60a4ec5a28950ddca11009
wangkun <kunwang1@jieyuechina.com>
2018-09-15 11:24:09 +0800

    activiti init

---
+++ activiti6-demo/src/main/java/com/example/activiti6demo/Activiti6DemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.activiti6demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Activiti6DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Activiti6DemoApplication.class, args);
+	}
+}
