a1c00f886f098a512db894d4fc7eba3cb89002c8
wangkun <kunwang1@jieyuechina.com>
2019-02-14 18:27:53 +0800

    redisson 并发测试

--- redis-redlock/src/main/java/com/example/redisredlock/config/RedissonConfig.java
+++ redis-redlock/src/main/java/com/example/redisredlock/config/RedissonConfig.java
@@ -3,31 +3,49 @@ package com.example.redisredlock.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
-import org.redisson.spring.data.connection.RedissonConnectionFactory;
-import org.springframework.beans.factory.annotation.Value;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
-
-import java.io.IOException;
 
 /**
  * @author WangKun
  * @create 2018-11-30
- * @desc
+ * @desc redisson配置可分为两种方式，1.yaml；2.javaconfig
+ * redisson 支持单机，主从，哨兵，集群等模式
  **/
-//@Configuration
+@Configuration
 public class RedissonConfig {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RedissonConfig.class);
 
     @Bean
-    public RedissonConnectionFactory redissonConnectionFactory(RedissonClient redisson) {
-        return new RedissonConnectionFactory(redisson);
-    }
-
-    @Bean(destroyMethod = "shutdown")
-    public RedissonClient redissonClient(@Value("classpath:/redisson.yaml") Resource configFile) throws IOException {
-        Config config = Config.fromYAML(configFile.getInputStream());
+    RedissonClient redissonSentinel() {
+        // 此为单机模式
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress("redis://192.168.69.52:6388");
+        LOGGER.info("redissonSentinel:: init ok");
         return Redisson.create(config);
     }
+
+    //
+//    @Bean
+//    RedissonClient redissonSentinel() {
+//        //此为哨兵模式
+//        Config config = new Config();
+//        config.useSentinelServers()
+//                .setMasterName("master6388")
+//                .addSentinelAddress("redis://127.0.0.1:26388");
+////                .setPassword("123456");
+//        return (Redisson) Redisson.create(config);
+//    }
+
+
+//    @Bean
+//    public RedissonClient redisson() throws IOException {
+//        // 本例子使用的是yaml格式的配置文件，读取使用Config.fromYAML，如果是Json文件，则使用Config.fromJSON
+//        Config config = Config.fromYAML(RedisRedlockApplication.class.getClassLoader().getResource("redisson.yaml"));
+//        return Redisson.create(config);
+//    }
 
 }
--- redis-redlock/src/test/java/com/example/redisredlock/RedisRedlockApplicationTests.java
+++ redis-redlock/src/test/java/com/example/redisredlock/RedisRedlockApplicationTests.java
@@ -1,23 +1,18 @@
 package com.example.redisredlock;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.redisson.api.RAtomicLong;
-import org.redisson.api.RBucket;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.*;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.concurrent.TimeUnit;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -25,139 +20,48 @@ public class RedisRedlockApplicationTests {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RedisRedlockApplicationTests.class);
 
-    @Autowired(required = false)
-    RedisLocker distributedLocker;
-
     @Autowired
     private RedissonClient redissonClient;
 
-    @Test
-    public void test() {
-// 设置字符串
-        RBucket<String> keyObj = redissonClient.getBucket("k6");
-        keyObj.set("v5236");
-    }
-
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+    String lockKey = "testRedisson";//分布式锁的key
 
     @Test
-    public void contextLoads() throws InterruptedException {
-//        for (int i = 0; i < 1; ++i) { // create and start threads
-//            System.out.println("----------------"+i);
-        new Thread(new Worker()).start();
-//        }
-
-//        for (int i = 0; i < 5; i++) {
-//            ()->{
-//                String threadName = Thread.currentThread().getName();
-//            }
-//        }
-        Thread.sleep(5000);
-//        doneSignal.await();
-        System.out.println("All processors done. Shutdown connection");
-    }
-
-
-    class Worker implements Runnable {
-        public void run() {
-            try {
-                distributedLocker.lock("WK", new AquiredLockWorker<Object>() {
-
-                    @Override
-                    public Object invokeAfterLockAquire() {
-                        return null;
-                    }
-
-                });
-            } catch (Exception e) {
-
-            }
-        }
-
+    public void test165515() {
+        // 设置一个key，aaa商品的库存数量为100
+        stringRedisTemplate.opsForValue().set("aaa", "100");
+        Assert.assertEquals("100", stringRedisTemplate.opsForValue().get("aaa"));
     }
 
 
     @Test
-    public void test1() {
-        RAtomicLong rAtomicLong = redissonClient.getAtomicLong("RAtomicLong");
-
-        Callable<Long> callable = () -> {
-            String threadName = Thread.currentThread().getName();
-            // test
-            long count = rAtomicLong.addAndGet(1);
-            System.out.printf("thread:{%s} >>> %d\n", threadName, count);
-            return count;
-        };
-
-        try {
-            ThreadPool.invoke(callable, 100);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            ThreadPool.shutdown();
+    public void testDistributed() {
+        //执行的业务代码
+        for (int i = 0; i < 55; i++) {
+            RLock lock = redissonClient.getLock(lockKey);
+            lock.lock(60, TimeUnit.SECONDS); //设置60秒自动释放锁  （默认是30秒自动过期）
+            int stock = Integer.parseInt(stringRedisTemplate.opsForValue().get("aaa").toString());
+            if (stock > 0) {
+                stringRedisTemplate.opsForValue().set("aaa", (stock - 1) + "");
+                System.out.println("test2_:lockkey:" + lockKey + ",stock:" + (stock - 1) + "");
+            }
+            lock.unlock(); //释放锁
         }
-
     }
 
     @Test
-    public void test2() {
-        RLock rLock = redissonClient.getLock("WKRLock");
-        Map<String, Integer> map = new HashMap() {{
-            put("number", 0);
-        }};
-
-        Callable<Void> callable = () -> {
-            rLock.lock();
-            try {
-                map.put("number", map.get("number") + 1);
-            } finally {
-                rLock.unlock();
+    public void testDistributed2() {
+        //执行的业务代码
+        for (int i = 0; i < 55; i++) {
+            RLock lock = redissonClient.getLock(lockKey);
+            lock.lock(60, TimeUnit.SECONDS); //设置60秒自动释放锁  （默认是30秒自动过期）
+            int stock = Integer.parseInt(stringRedisTemplate.opsForValue().get("aaa").toString());
+            if (stock > 0) {
+                stringRedisTemplate.opsForValue().set("aaa", (stock - 1) + "");
+                System.out.println("test2_:lockkey:" + lockKey + ",stock:" + (stock - 1) + "");
             }
-            return null;
-        };
-
-//        try {
-//            ThreadPool.invoke(callable, 100);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        } finally {
-            ThreadPool.shutdown();
-//        }
-        System.out.println("number=" + map.get("number"));
-    }
-
-
-    static class ThreadPool {
-        private volatile static ThreadPoolExecutor executor;
-
-        private static void initThreadPool() {
-            executor = new ThreadPoolExecutor(128, 128, 0, TimeUnit.SECONDS,
-                    new LinkedBlockingQueue<>());
-        }
-
-        public static <R> List<Future<R>> invoke(Callable<R> callable, int nRepeat) throws InterruptedException {
-            return invoke(callable, nRepeat, 0, TimeUnit.SECONDS);
-        }
-
-        public static <R> List<Future<R>> invoke(Callable<R> callable, int nRepeat, long timeout, TimeUnit unit) throws InterruptedException {
-            if (executor == null) {
-                initThreadPool();
-            }
-            List<Callable<R>> callables = IntStream.range(0, nRepeat).boxed().map(i -> callable).collect(Collectors.toList());
-            for (Callable<R> rCallable : callables) {
-                System.out.println(rCallable.hashCode());
-            }
-            if (timeout <= 0) {
-                return executor.invokeAll(callables);
-            }
-            return executor.invokeAll(callables, timeout, unit);
-        }
-
-
-        public static void shutdown() {
-            if (executor != null && !executor.isShutdown()) {
-                executor.shutdown();
-            }
-            executor = null;
+            lock.unlock(); //释放锁
         }
     }
 
